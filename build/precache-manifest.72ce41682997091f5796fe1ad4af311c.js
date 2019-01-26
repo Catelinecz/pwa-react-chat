139b025dd7bbe019c39407b1d69301c29a552a55
Kateřina Lebedová <lebedova@skeleton.cz>
2019-01-26 18:40:11 +0100

    heroku deploy

---
+++ build/precache-manifest.72ce41682997091f5796fe1ad4af311c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "28e850859e0bf0b68525",
+    "url": "/static/js/main.28e85085.chunk.js"
+  },
+  {
+    "revision": "9762c2eb154e6b18ebeb",
+    "url": "/static/js/1.9762c2eb.chunk.js"
+  },
+  {
+    "revision": "28e850859e0bf0b68525",
+    "url": "/static/css/main.355ec54a.chunk.css"
+  },
+  {
+    "revision": "5d326520333421fe64b5f382eb45f21f",
+    "url": "/index.html"
+  }
+];
