b88867ea150162b8f88346f5cb548964b2a23d46
Kateřina Lebedová <lebedova@skeleton.cz>
2019-01-27 18:39:19 +0100

    login page style

--- src/components/NewMessageForm.jsx
+++ src/components/NewMessageForm.jsx
@@ -25,9 +25,9 @@ export default class NewMessageForm extends React.Component {
     render() {
         return (
             <div className="message-input">
-                <form className="message-form" onSubmit={this.handleSubmit}>
+                <form className="message-form" autocomplete="off" onSubmit={this.handleSubmit}>
                     <input id="message" type="text" className="form-control" onChange={this.handleChange} placeholder="Type your message and press ENTER" value={this.state.message}/>
-                    <button disabled={this.state.message.length < 1} className="send" type="submit">Send</button>
+                    <button autocomplete="off" disabled={this.state.message.length < 1} className="send" type="submit">Send</button>
                 </form>
             </div>
         )
